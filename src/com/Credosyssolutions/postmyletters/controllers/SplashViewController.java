88d4baa174a7967bf37c175ab60614b82bae5594
RahulNair <rhlnair@gmail.com>
2014-09-06 02:13:07 +0530

    First Commit

---
+++ src/com/Credosyssolutions/postmyletters/controllers/SplashViewController.java
@@ -0,0 +1,69 @@
+package com.Credosyssolutions.postmyletters.controllers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.parse.*;
+import com.Credosyssolutions.postmyletters.R;
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+public class SplashViewController extends Activity {
+	
+	// Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_view_controller);
+		   Parse.initialize(this, "ikCHpUnuiBkVeAxkYI0IRWGsoesrs3SsO5Ah1ezV", "ohM5yMC8aHQwlAJSshOjD3TA0Qebgk8PjvqA4B8f");
+
+		if (ParseUser.getCurrentUser() == null) {
+			//show Loginscreen
+			
+		}else{
+			new Handler().postDelayed(new Runnable() {
+	            /*
+	             * Showing splash screen with a timer. This will be useful when you
+	             * want to show case your app logo / company
+	             */
+	            @Override
+	            public void run() {
+	                // This method will be executed once the timer is over
+	                // Start your app main activity
+	                Intent i = new Intent(SplashViewController.this, RootViewController.class);
+	                startActivity(i);
+	 
+	                // close this activity
+	                finish();
+	            }
+	        }, SPLASH_TIME_OUT);
+
+			
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_view_controller, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
